bbcbab154e6c831173cc2174777e7151ba7fecb2
Nivash23 <nivashsubash32@gmail.com>
2023-11-06 12:22:41 +0530

    contxtweb

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,9 @@
 import React from 'react'
-import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import './assets/App.css';
-import All from '../components/All';
-import Fullstack from '../components/Fullstack';
-import Datascience from '../components/Datascience';
-import Cyber from '../components/Cyber';
-import Career from '../components/Career';
+
 
 function App() {
-    const padding= {
-        padding:20,
-    }
     return (
-            
-      <Router>
-          <div id='navigation'>
-              <Link to='/' style={padding}>ALL</Link>
-              <Link to='/fullstack' style={padding}>FULL STACK DEVELOPEMENT</Link>
-              <Link to='/data' style={padding}>DATA SCIENCE</Link>
-              <Link to='/cyber' style={padding}>CYBER SECURITY</Link>
-              <Link to='/career' style={padding}>CAREER</Link>
-          </div>
-          <Routes>
-              <Route path='/' element={<All/>}/>
-              <Route  path='/fullstack' element={<Fullstack/>}/>
-              <Route path='/data' element={<Datascience/>}/>
-              <Route path='/cyber' element={<Cyber/>}/>
-              <Route path='/career' element={<Career/>}/>
-          </Routes>
-    </Router>
+    <div>App</div>
   )
 }
 
