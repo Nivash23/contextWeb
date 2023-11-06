c2ce48a334bbcb5829afa13c5bf70bca2e1ccf52
Nivash23 <nivashsubash32@gmail.com>
2023-11-07 05:28:58 +0530

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React, { createContext, useState } from 'react'
-import Child from '../components/child'
 
 
 const MessageContext = createContext();
 function App() {
-    const [message,setMessage]=useState('hi')
     return (
         <div>
-            <MessageContext.Provider value={{ message }}>
-                <Child />
+            <MessageContext.Provider>
+                
 
             </MessageContext.Provider>
     </div>
